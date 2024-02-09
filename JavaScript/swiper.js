d8b034b12b3c5e8e1877bd8e4bd19968a85de9af
AudiHazael <hazaelaudi@gmail.com>
2024-02-09 17:27:05 +0100

    Swiper Updates

--- JavaScript/swiper.js
+++ JavaScript/swiper.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1, // Display one slide at a time
-    spaceBetween: 1000, // Adjust the spacing between slides
+    spaceBetween: 1500, // Adjust the spacing between slides
     pagination: {
       el: '.swiper-pagination', // Specify the pagination element
       clickable: true, // Allow users to click on pagination dots to navigate
     },
     autoplay: {
-      delay: 5000, // Automatically advance slides every 5 seconds
+      delay: 15000, // Automatically advance slides every 15 seconds
       disableOnInteraction: false, // Continue autoplay even when the user interacts with the slider
     },
     // Change the effect to 'slide' for a sliding animation
